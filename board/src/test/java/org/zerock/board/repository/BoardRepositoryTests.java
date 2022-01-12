4d73d9e94be20699b5417d40a314df7395be8f82
hcpak <jasotn12@naver.com>
2022-01-12 22:17:10 +0900

    feat: @ManyToOne과 Eager/Lazy loading

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -27,5 +29,16 @@ public class BoardRepositoryTests {
 
             boardRepository.save(board);
         });
+    }
+
+    @Transactional
+    @Test
+    public void testRead1() {
+        Optional<Board> result = boardRepository.findById(100L);
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
     }
 }
