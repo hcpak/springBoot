6cd8ad3f7b25189f71a56a8a031616a8ccc0760b
hcpak <jasotn12@naver.com>
2022-01-12 15:38:39 +0900

    feat: 검색 처리

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -44,4 +44,28 @@ public class GuestbookServiceTests {
         System.out.println("==================================");
         resultDTO.getPageList().forEach(System.out::println);
     }
+
+    @Test
+    public void testSearch() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc")
+                .keyword("한글")
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " +resultDTO.isPrev());
+        System.out.println("NEXT: " +resultDTO.isNext());
+        System.out.println("TOTAL: " +resultDTO.getTotalpage());
+
+        System.out.println("----------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("==================================");
+        resultDTO.getDtoList().forEach(System.out::println);
+    }
 }
