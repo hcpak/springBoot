076c44be260d026f8b9afef6df7d54d4658ead33
hcpak <jasotn12@naver.com>
2022-01-13 00:50:06 +0900

    feat: QuerydslRepositorySupport 생성 및 동작 확인

---
+++ board/src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -0,0 +1,39 @@
+package org.zerock.board.repository.search;
+
+import com.querydsl.jpa.JPQLQuery;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.QBoard;
+import org.zerock.board.entity.QReply;
+
+import java.util.List;
+
+@Log4j2
+public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
+
+    public SearchBoardRepositoryImpl() {
+        super(Board.class);
+    }
+
+    @Override
+    public Board search1(){
+        log.info("search1.......................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        jpqlQuery.select(board).where(board.bno.eq(1L));
+
+        log.info("----------------------");
+        log.info(jpqlQuery);
+        log.info("----------------------");
+
+        List<Board> result = jpqlQuery.fetch();
+
+        return null;
+    }
+}
