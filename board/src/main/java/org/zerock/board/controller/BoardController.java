a6922452e6547a0f2bf62b18804c85175accba65
hcpak <jasotn12@naver.com>
2022-01-13 00:08:53 +0900

    feat: 복록 컨트롤러/화면 처리 구현

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -2,8 +2,12 @@ package org.zerock.board.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.board.dto.PageRequestDTO;
 import org.zerock.board.service.BoardService;
 
 @Controller
@@ -13,4 +17,11 @@ import org.zerock.board.service.BoardService;
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list................ " + pageRequestDTO);
+
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
 }
