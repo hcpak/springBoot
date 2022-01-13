87114cc9c51cee55d7321507f96fad94e5b07030
hcpak <jasotn12@naver.com>
2022-01-13 15:12:20 +0900

    feat: thumbnail 생성

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -58,6 +59,12 @@ public class UploadController {
 
             try {
                 uploadFile.transferTo(savePath);
+
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100,100);
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
@@ -74,7 +81,7 @@ public class UploadController {
         try{
             String srcFileName = URLDecoder.decode(fileName, "UTF-8");
 
-            log.info("fileNmae: " + srcFileName);
+            log.info("fileName: " + srcFileName);
 
             File file = new File(uploadPath + File.separator + srcFileName);
 
