b0a295704635293d35cf17f996846655fac577b5
hcpak <jasotn12@naver.com>
2022-01-24 13:44:24 +0900

    feat: AuthenticationManager 설정

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        //사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$fYwaQUpUft4LmNL8cJ1HIue.T6eqas1hotzHaNNwN9EoR2s7r/Z2G")
+                .roles("USER");
     }
 
 }
