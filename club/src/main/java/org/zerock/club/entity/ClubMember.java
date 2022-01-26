c58996458ab0645b063557caabca157218ba2a1e
hcpak <jasotn12@naver.com>
2022-01-26 09:35:10 +0900

    feat: Club member 생성

---
+++ club/src/main/java/org/zerock/club/entity/ClubMember.java
@@ -0,0 +1,24 @@
+package org.zerock.club.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class ClubMember extends BaseEntity{
+
+    @Id
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private boolean fromSocial;
+}
